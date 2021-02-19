28f9020909d920664f71da5f69f3d0a4211df7ed
raulregadera <raulregadera@gmail.com>
2021-02-19 20:03:55 +0100

    Basic player controls

---
+++ Assets/Scripts/MobPlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobPlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public int vel;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float temp = Input.acceleration.x;
+        Debug.Log(temp);
+
+        rb.AddForce(new Vector2 (Input.acceleration.x, 0 )* vel);
+
+        if (Input.GetMouseButton(0) && rb.transform.position.y < 3)
+        {
+            rb.AddForce(transform.up * 2.5f * vel);
+        }
+
+        else if (rb.position.y > -2)
+        {
+            rb.AddForce(transform.up * -2.5f * vel);
+        }
+
+
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public int vel;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A) && rb.transform.position.x > -2)
+        {
+            rb.AddForce(transform.right * -vel);
+        }
+
+        else if (Input.GetKey(KeyCode.D) && rb.transform.position.x < 2)
+        {
+            rb.AddForce(transform.right * vel);
+        }
+
+        if (Input.GetKey(KeyCode.W) && rb.transform.position.y < 3)
+        {
+            rb.AddForce(transform.up * 2.5f * vel);
+        }
+
+        else if ( rb.position.y > -2)
+        {
+            rb.AddForce(transform.up * -2.5f * vel);
+        }
+
+
+    }
+}
